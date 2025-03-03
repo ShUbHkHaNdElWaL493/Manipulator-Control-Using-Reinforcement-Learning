3c37d0017e77fdaa834abf2aefcc07603aa6e7ab
Shubh Khandelwal <shubh4664@gmail.com>
2025-03-03 10:26:22 +0530

    Initial commit

---
+++ install/manipulator/share/manipulator/launch/viewer.launch.py
@@ -0,0 +1,39 @@
+#   CS22B1090
+#   Shubh Khandelwal
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+import os
+
+def generate_launch_description():
+
+    package_dir = get_package_share_directory('manipulator')
+
+    urdf_path = os.path.join(package_dir, 'urdf', 'manipulator.urdf')
+    rviz_config_path = os.path.join(package_dir, 'rviz', 'manipulator_config.rviz')
+
+    return LaunchDescription([
+        DeclareLaunchArgument('urdf_file', default_value=urdf_path),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            arguments=[urdf_path],
+            output='screen'
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            # arguments=['-d', rviz_config_path],
+            output='screen'
+        )
+    
+    ])
